1a02d3982abbb01813009dcb3b835b9326258cdf
mechatroid <graur_victor@yahoo.com>
2026-03-09 16:50:35 -0400

    10 out of 10 server.py pylint

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+''' Executing this function initiates the application of emotion
+    detector to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,12 +10,23 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_detector():
+    ''' This code receives the text from the HTML interface and 
+        runs emotion detector over it using emotion_detector()
+        function. The output returned shows the emotions and their confidence 
+        score for the provided text.
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    return "For the given statement, the system response is \'anger\': {}, \'disgust\': {}, \'fear\': {}, \'joy\': {} and \'sadness\': {}. The dominant emotion is {}".format(response['anger'], response['disgust'], response['fear'], response['joy'], response['sadness'], response['dominant_emotion'])
+    return (f"For the given statement, the system response is \'anger\': {response['anger']},"
+    f" \'disgust\': {response['disgust']}, \'fear\': {response['fear']}, "
+    f" \'joy\': {response['joy']} and \'sadness\': {response['sadness']}. "
+    f" The dominant emotion is {response['dominant_emotion']}")
 
 @app.route("/")
 def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
